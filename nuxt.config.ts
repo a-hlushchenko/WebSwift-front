fa099fc50f4a76d39e9af15c1428feb4199b8538
a-hlushchenko <glantoshka@gmail.com>
2025-01-19 01:10:22 +0200

    seo upd

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -22,8 +22,7 @@ export default defineNuxtConfig({
         { property: "og:title", content: "WebSwift" },
         {
           name: "viewport",
-          content:
-            "width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no",
+          content: "width=device-width, initial-scale=1.0",
         },
       ],
     },
